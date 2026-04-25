c35382eb8465630a1564d1c00a1b4196d168500a
rexram987-create <rexram987@gmail.com>
2026-04-25 16:40:01 +0300

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function App() {
-  return (
-    <div style={{color: "white", padding: "40px", fontSize: "20px"}}>
-      האתר עובד 🎉
-    </div>
-  );
-}
